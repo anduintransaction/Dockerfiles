0206ba725c22a6f57ea3bd621c3c2409d12afede
Binh Nguyen <ngbinh@gmail.com>
2016-06-02 12:22:57 +0700

    Add sync_gateway

---
+++ sync_gateway/start_sync_gateway.sh
@@ -0,0 +1,6 @@
+# we need to set the permissions here because docker mounts volumes as root
+
+chown -R couchbase:couchbase /opt/anduin/sync_gateway/
+
+# Run everything
+/usr/bin/supervisord --configuration /etc/supervisor/supervisord.conf
