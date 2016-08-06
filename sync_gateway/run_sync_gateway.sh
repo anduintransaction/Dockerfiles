f58c6faa553d4306a1047d4f6a1446aa1de6ebb6
keimoon <keimoonvie@gmail.com>
2016-08-06 12:50:10 +0700

    Change sync gateway docker to use rpm

--- sync_gateway/run_sync_gateway.sh
+++ sync_gateway/run_sync_gateway.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-    /usr/local/bin/sync_gateway /opt/anduin/couchbase/config.json
+    /opt/couchbase-sync-gateway/bin/sync_gateway /opt/anduin/couchbase/config.json
     if [ $? -ne 0 ]; then
         echo "Restarting..."
         sleep 3
