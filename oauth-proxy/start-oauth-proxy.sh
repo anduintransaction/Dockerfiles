605f5d9912402a913529f309a2cbc2a3efff229b
Tam. Nguyen Duc <keimoonvie@gmail.com>
2016-08-16 10:19:02 +0700

    Remove nginx in oauth proxy (#34)

--- oauth-proxy/start-oauth-proxy.sh
+++ oauth-proxy/start-oauth-proxy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-envsubst '${NGINX_BASIC_AUTH} ${NGINX_PROXY}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
 /usr/bin/supervisord --configuration /etc/supervisor/supervisord.conf
