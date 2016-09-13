ea5e5cf35c7cd35bfeba29cc387d552172b3ac1c
keimoon <keimoonvie@gmail.com>
2016-09-13 14:49:39 +0700

    Added td-agent dockerfile (use consul template)

--- graylog/td-agent/start-td-agent.sh
+++ graylog/td-agent/start-td-agent.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 echo "{{key \"$CONSUL_KEY\"}}" > /tmp/td-agent.conf.tmpl
+echo "Connect to $CONSUL_ENDPOINT"
+printf "Waiting for consul."
+until $(curl --output /dev/null --silent --head --fail http://$CONSUL_ENDPOINT/v1/agent/self); do
+    printf "."
+    sleep 1
+done
+echo "done"
+
+consul-template -consul $CONSUL_ENDPOINT -template "/tmp/td-agent.conf.tmpl:/etc/td-agent/td-agent.conf:/etc/init.d/td-agent restart"
