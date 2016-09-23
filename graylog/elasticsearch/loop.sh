9b8e0bd3095fc688a621da4a6f890942fddcc235
keimoon <keimoonvie@gmail.com>
2016-09-23 16:27:47 +0700

    Added graylog

--- graylog/elasticsearch/loop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-while true; do sleep 60; done
--- graylog/graylog/run.sh
+++ graylog/graylog/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "{{key \"$CONSUL_KEY\"}}" > /tmp/elasticsearch.yml.tmpl
+echo "{{key \"$CONSUL_KEY\"}}" > /tmp/graylog.conf.tmpl
 echo "Connect to $CONSUL_ENDPOINT"
 printf "Waiting for consul."
 until $(curl --output /dev/null --silent --head --fail http://$CONSUL_ENDPOINT/v1/agent/self); do
@@ -9,4 +9,6 @@ until $(curl --output /dev/null --silent --head --fail http://$CONSUL_ENDPOINT/v
 done
 echo "done"
 
-consul-template -consul $CONSUL_ENDPOINT -template "/tmp/elasticsearch.yml.tmpl:/etc/elasticsearch/elasticsearch.yml:/etc/init.d/elasticsearch restart"
+consul-template -consul $CONSUL_ENDPOINT -template \
+     "/tmp/graylog.conf.tmpl:/etc/graylog/server/server.conf:/graylog restart"
+
