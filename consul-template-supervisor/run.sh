d89c0690871cfa20610d968a167589f641374983
Tam. Nguyen Duc <keimoonvie@gmail.com>
2016-10-06 16:47:03 +0800

    Add kong (#51)

---
+++ consul-template-supervisor/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "{{key \"$CONSUL_KEY\"}}" > /tmp/app.tmpl
+echo "Connect to $CONSUL_ENDPOINT"
+printf "Waiting for consul."
+until $(curl --output /dev/null --silent --head --fail http://$CONSUL_ENDPOINT/v1/agent/self); do
+    printf "."        
+    sleep 1 
+done
+echo "done"
+
+consul-template -consul $CONSUL_ENDPOINT \
+                -template "/tmp/app.tmpl:$CONSUL_APP_CONFIG" \
+                -exec "supervisord -c /etc/supervisord.conf"
