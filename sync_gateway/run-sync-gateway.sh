1c6bbbf2352fe835dd2cee5da512eba047c8971a
Tam. Nguyen Duc <keimoonvie@gmail.com>
2016-10-31 00:17:21 +0700

    Fix sync gateway running in consul mode (#84)

--- sync_gateway/run-sync-gateway.sh
+++ sync_gateway/run-sync-gateway.sh
@@ -2,31 +2,27 @@
 
 SYNC_GATEWAY_CONFIG_FILE=/opt/sync-gateway-config.json
 PID=0
-KILLING=0
 
 function _quit {
-    if [ "$PID" -ne 0 ]; then
-        echo "Killing $PID"
-        KILLING=1
-        kill $PID
-    fi
+    echo "Killing $PID FROM $$"
+    KILLING=1        
+    kill -0 $PID > /dev/null 2>&1 && kill $PID
+    echo "Done killing FROM $$"
+    exit 0
 }
 
 trap _quit SIGTERM
 
 while true; do
+    echo "Try to start FROM $$"
     if [ -f "$SYNC_GATEWAY_CONFIG_FILE" ]; then
         /opt/sync_gateway $SYNC_GATEWAY_CONFIG_FILE &
     else
         /opt/sync_gateway &
     fi
     PID=$!
-    echo "waiting for $PID"
+    echo "waiting for $PID FROM $$"
     wait $PID
-    if [ $KILLING -ne 0 ]; then
-        echo "got kill signal, exiting"
-        break
-    fi
-    echo "something wrong, restarting"
+    echo "something wrong, restarting FROM $$"
     sleep 3
 done
