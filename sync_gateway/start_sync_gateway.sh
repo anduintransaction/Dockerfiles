77012f2313561518710a7c4215bdd9e1d9a0e196
Tam. Nguyen Duc <keimoonvie@gmail.com>
2016-10-30 10:27:41 +0700

    Update sync gateway for new deployment (#83)

--- sync_gateway/start_sync_gateway.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# we need to set the permissions here because docker mounts volumes as root
-chown -R couchbase:couchbase /opt/anduin/sync_gateway/
-
-# Run everything
-/usr/bin/supervisord --configuration /etc/supervisor/supervisord.conf
