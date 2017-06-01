16f366af2f7ffc06ef1e4cf72903c67904863dcf
Tam. Nguyen Duc <keimoonvie@gmail.com>
2017-06-01 11:07:27 +0700

    Add keycloak and postgres-wait (#129)

---
+++ keycloak/run-keycloak.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+function checkEnv {
+    if [ -z "$KEYCLOAK_ADMIN_USER" ]; then
+        export KEYCLOAK_ADMIN_USER=admin
+        echo "Using default keycloak admin user: $KEYCLOAK_ADMIN_USER"
+    else
+        echo "Using keycloak admin user: $KEYCLOAK_ADMIN_USER"
+    fi
+    if [ -z "$KEYCLOAK_ADMIN_PASSWORD" ]; then
+        export KEYCLOAK_ADMIN_PASSWORD=admin
+        echo "Using default keycloak admin password: $KEYCLOAK_ADMIN_PASSWORD"
+    else
+        echo "Using keycloak admin password: ${KEYCLOAK_ADMIN_PASSWORD:0:4}******"
+    fi
+    if [ -z "$KEYCLOAK_POSTGRES_HOST" ]; then
+        export KEYCLOAK_POSTGRES_HOST="postgres"
+        echo "Using default postgres host: $KEYCLOAK_POSTGRES_HOST"
+    else
+        echo "Using postgres host: $KEYCLOAK_POSTGRES_HOST"
+    fi
+    if [ -z "$KEYCLOAK_POSTGRES_PORT" ]; then
+        export KEYCLOAK_POSTGRES_PORT="5432"
+        echo "Using default postgres port: $KEYCLOAK_POSTGRES_PORT"
+    else
+        echo "Using postgres port: $KEYCLOAK_POSTGRES_PORT"
+    fi
+    if [ -z "$KEYCLOAK_POSTGRES_USER" ]; then
+        export KEYCLOAK_POSTGRES_USER="keycloak"
+        echo "Using default postgres user: $KEYCLOAK_POSTGRES_USER"
+    else
+        echo "Using postgres user: $KEYCLOAK_POSTGRES_USER"
+    fi
+    if [ -z "$KEYCLOAK_POSTGRES_PASSWORD" ]; then
+        export KEYCLOAK_POSTGRES_PASSWORD="keycloak"
+        echo "Using default postgres password: $KEYCLOAK_POSTGRES_PASSWORD"
+    else
+        echo "Using postgres password: $KEYCLOAK_POSTGRES_PASSWORD"
+    fi
+    if [ -z "$KEYCLOAK_POSTGRES_DATABASE" ]; then
+        export KEYCLOAK_POSTGRES_DATABASE="keycloak"
+        echo "Using default postgres database: $KEYCLOAK_POSTGRES_DATABASE"
+    else
+        echo "Using postgres database: $KEYCLOAK_POSTGRES_DATABASE"
+    fi
+}
+
+if [ $# -lt 1 ]; then
+    exec /bin/bash
+fi
+
+if [ $1 != "server" ]; then
+    exec $@
+fi
+
+here=`cd $(dirname $BASH_SOURCE); pwd`
+checkEnv
+
+ipAddress=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
+if [ -z "$ipAddress" ]; then
+    echo "Cannot determine local ip address"
+    exit 1
+fi
+
+echo "Using ip address: $ipAddress"
+
+$here/add-user-keycloak.sh --user $KEYCLOAK_ADMIN_USER --password $KEYCLOAK_ADMIN_PASSWORD &&
+    exec $here/standalone.sh -Djboss.bind.address.private=$ipAddress -b 0.0.0.0 --server-config standalone-ha.xml
---
+++ postgres-wait/wait.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Using env:"
+echo "PGHOST=$PGHOST"
+echo "PGUSER=$PGUSER"
+echo "PGPASSWORD=${PGPASSWORD:0:4}****"
+
+count=0
+until psql -c 'SELECT 1' > /dev/null; do
+    echo .
+    count=`expr $count + 1`
+    if [ $count -gt 10 ]; then
+        echo "Unable to connect to postgres server"
+        exit 1
+    fi
+    sleep 5
+done
+echo "Done"
