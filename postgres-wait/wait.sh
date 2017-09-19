6a46020b82398febede6d55498edea4505095018
Tam. Nguyen Duc <keimoonvie@gmail.com>
2017-09-19 15:29:34 +0700

    Update postgres wait (#149)

--- postgres-wait/wait.sh
+++ postgres-wait/wait.sh
@@ -4,16 +4,19 @@ echo "Using env:"
 echo "PGHOST=$PGHOST"
 echo "PGUSER=$PGUSER"
 echo "PGPASSWORD=${PGPASSWORD:0:4}****"
-export PGCONNECT_TIMEOUT=10
+export PGCONNECT_TIMEOUT=3
 
+timeBegin=`echo $(($(date +%s%N)/1000000))`
 count=0
 until psql -c 'SELECT 1' > /dev/null; do
     echo .
     count=`expr $count + 1`
-    if [ $count -gt 10 ]; then
+    if [ $count -gt 30 ]; then
         echo "Unable to connect to postgres server"
         exit 1
     fi
-    sleep 5
+    sleep 2
 done
-echo "Done"
+timeEnd=`echo $(($(date +%s%N)/1000000))`
+timeElapsed=`expr $timeEnd - $timeBegin`
+echo "Done, elapsed time is $timeElapsed"
