9cac427f9e61f66054547d7b407a17d0d5f1d4f3
keimoon <keimoonvie@gmail.com>
2018-02-02 11:14:31 +0700

    Add configuration for logstash worker size

--- elk/logstash/run-logstash.sh
+++ elk/logstash/run-logstash.sh
@@ -11,4 +11,9 @@ fi
 sed -i 's/-Xms1g/-Xms'$LOGSTASH_HEAP_SIZE'/' /opt/logstash/config/jvm.options
 sed -i 's/-Xmx1g/-Xmx'$LOGSTASH_HEAP_SIZE'/' /opt/logstash/config/jvm.options
 
-exec /opt/logstash/bin/logstash -f $LOGSTASH_CONFIG_FILE
+LOGSTASH_OPTS="-f $LOGSTASH_CONFIG_FILE"
+if [ ! -z $LOGSTASH_WORKER_SIZE ]; then
+    LOGSTASH_OPTS="$LOGSTASH_OPTS -w $LOGSTASH_WORKER_SIZE"
+fi
+
+exec /opt/logstash/bin/logstash $LOGSTASH_OPTS
